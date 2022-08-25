0bb7f4026a83a7b5e58e8a5654c3f686e5f63ba4
Syafiq Naim <syafiqnaim@Syafiqs-MacBook-Pro.local>
2022-08-25 11:20:37 +0800

    hello2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return  "hello good sir v3.0. today's date is 25082022";
+    return  "hello good sir v3.0. today's date is 25082022-1";
 });

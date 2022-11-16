6759d9d5d38b35c1c02cf8e9cf408443f5d05a3f
Syafiq Naim <syafiqnaim@Syafiqs-MacBook-Pro.local>
2022-11-16 16:10:57 +0800

    16112022-3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return  "hello good day sir, today's date is 16112022-2";
+    return  "hello good day sir, today's date is 16112022-3";
 });
